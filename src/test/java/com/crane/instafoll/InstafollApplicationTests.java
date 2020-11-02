1e5ee2d85c4c24e8dbcd6580fb058b9bc9c91fea
stanslav <partner.stanislav.zhuavel@philips.com>
2020-11-03 00:11:43 +0200

    fix test

--- src/test/java/com/crane/instafoll/InstafollApplicationTests.java
+++ src/test/java/com/crane/instafoll/InstafollApplicationTests.java
@@ -4,13 +4,15 @@ import com.crane.instafoll.services.InstaActionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootTest
 class InstafollApplicationTests {
 
     @MockBean
     InstaActionService instaActionService;
-
+    @MockBean
+    TelegramBotsApi telegramBotsApi;
     @Test
     void contextLoads() {
     }
