f3a2bf043f3cba4c368cf21580a0dcf3cd0fec7b
stanslav <partner.stanislav.zhuavel@philips.com>
2020-11-21 13:13:48 +0200

    health action number param

--- src/main/java/com/crane/instafoll/jobs/JobParams.java
+++ src/main/java/com/crane/instafoll/jobs/JobParams.java
@@ -3,7 +3,6 @@ package com.crane.instafoll.jobs;
 import com.github.instagram4j.instagram4j.IGClient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -12,6 +11,8 @@ public class JobParams {
     private String userName;
 
     private int maxActionNumber;
+
+    private int actionsPerformed;
 
     private int intervalInSeconds;
 
