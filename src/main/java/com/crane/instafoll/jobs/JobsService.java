4bca0ea2ff8d8fa8e903547a67c4e5f5d997fc29
stanslav <partner.stanislav.zhuavel@philips.com>
2020-11-21 03:04:51 +0200

    refactor

--- src/main/java/com/crane/instafoll/jobs/JobsService.java
+++ src/main/java/com/crane/instafoll/jobs/JobsService.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.*;
+import static java.util.Arrays.asList;
 
 @AllArgsConstructor
 @Component
